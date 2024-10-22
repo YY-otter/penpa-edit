87a5ed8c417316658fbb8062c638f3084de03d40
NotHotmilk <148937409+NotHotmilk@users.noreply.github.com>
2024-10-22 21:56:48 +0900

    Update style.js

--- docs/js/style.js
+++ docs/js/style.js
@@ -291,7 +291,7 @@ function set_font_style(ctx, size, type, ccolor = "none") {
     ctx.textAlign = "center";
     ctx.textBaseline = "alphabetic";
     ctx.setLineDash([]);
-    var fontfamily = "Helvetica,Arial";
+    var fontfamily = "MS PGothic,Helvetica,Arial";
     ctx.font = size + "px " + fontfamily;
     switch (type) {
         case 0:
